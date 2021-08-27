ddf6a59e255acc84c1ffea0364e862fdd99b11ae
Apu <rahman_apu@outlook.com>
2021-08-28 00:26:42 +0600

    add functionality to the calculate

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const currencyOne = document.querySelector("#currency-one .country-code");
+const inputOne = document.querySelector("#currency-one input");
+const currencyTwo = document.querySelector("#currency-two .country-code");
+const inputTwo = document.querySelector("#currency-two input");
+const rate = document.querySelector("#swap #rate");
+
+async function calculate() {
+  const firstCountryCode = currencyOne.value;
+  const secondCountryCode = currencyTwo.value;
+
+  const getData = await fetch(
+    `https://v6.exchangerate-api.com/v6/83ab944c75ffc3e064e5c850/latest/${firstCountryCode}`
+  );
+  const response = await getData.json();
+  const value = await response.conversion_rates[secondCountryCode];
+  rate.innerHTML = `1 ${firstCountryCode} = ${value} ${secondCountryCode} `;
+
+  inputTwo.value = (inputOne.value * value).toFixed(2);
+}
+
+calculate();
+currencyOne.addEventListener("change", calculate);
+currencyTwo.addEventListener("change", calculate);
+inputOne.addEventListener("input", calculate);
+inputTwo.addEventListener("input", calculate);
